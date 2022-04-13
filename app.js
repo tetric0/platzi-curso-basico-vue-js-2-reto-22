2cf118ff908cdb76cda76db348aee8e5a55efadc
victor.munoz <victor.munoz@upc.edu>
2022-04-13 16:09:48 +0200

    Reto del video 22 del 'Curso Bàsico de Vue.js 2' completado.

---
+++ app.js
@@ -0,0 +1,41 @@
+Vue.component('modal', {
+    props: ['title'],
+
+    methods: {
+        close () {
+            this.$emit('close')
+        }
+    },
+    
+    template: `
+        <div class="modal-mask">
+            <div class="modal-wrapper">
+                <div class="modal-container">
+                    <h3>{{ title }}</h3>
+                    <div>
+                        <slot name="body"></slot>
+                    </div>
+                    <footer>
+                        <button v-on:click="close">Cerrar</button>
+                    </footer>
+                </div>
+            </div>
+        </div>`
+})
+
+new Vue({
+    el: '#app',
+
+    data () {
+        return {
+            title: 'Warning',
+            showModal: false
+        }
+    },
+
+    methods: {
+        toggleModal () {
+            this.showModal = !this.showModal
+        }
+    }
+})
